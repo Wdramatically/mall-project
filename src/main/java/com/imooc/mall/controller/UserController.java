e7b256a6c5144a56faf40c16052cf89cf9ff81df
hxd <982304089@qq.com>
2021-07-04 21:46:53 +0800

    test Mybatis connection

---
+++ src/main/java/com/imooc/mall/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.imooc.mall.controller;
+
+import com.imooc.mall.model.pojo.User;
+import com.imooc.mall.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/test")
+    @ResponseBody
+    public User getUser(@RequestParam Integer id){
+        return userService.getUser(id);
+    }
+}
---
+++ src/main/java/com/imooc/mall/service/UserService.java
@@ -0,0 +1,8 @@
+package com.imooc.mall.service;
+
+
+import com.imooc.mall.model.pojo.User;
+
+public interface UserService {
+    User getUser(Integer userId);
+}
